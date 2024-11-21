4d239c411b8589b1c384e245e0a5b5deed967160
Mikely Barboza <barbozamikely@gmail.com>
2024-11-21 16:28:30 -0300

    Data

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 function updateClock() {
     const clockElement = document.getElementById('clock');
+    const dateElement = document.getElementById('date');
     const now = new Date();
+
     const hours = String(now.getHours()).padStart(2, '0');
     const minutes = String(now.getMinutes()).padStart(2, '0');
     const seconds = String(now.getSeconds()).padStart(2, '0');
+
+    const day = String(now.getDate()).padStart(2, '0');
+    const month = String(now.getMonth()).padStart(2, '0');
+    const year = now.getFullYear();
+
     clockElement.textContent = `${hours}:${minutes}:${seconds}`;
+    dateElement.textContent = `${day}/${month}/${year}`;
 }
 
 // Atualiza o relógio a cada segundo
